b166626a8a0422cb2620cf4843c7260e7d5300f0
Lurchfresser <Tritrop007@gmx.de>
2023-08-28 10:39:13 +0200

    implemented transpo table and move ordering, easy endgames and fixed alpha beta bug

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -9,21 +9,82 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
+
+
         Board board;
 
         int[] values = { 0, 100, 300, 320, 500, 900, 0 };
+
+        int getPieceValue(PieceType pieceType)
+        {
+            return values[(int)pieceType];
+        }
+
+
+        struct transposition
+        {
+            public ulong zobristHash;
+            public sbyte depth;
+            public int eval;
+            public bool initialized;
+        }
+
+        List<transposition>[] transpositiontable = new List<transposition>[256];
+
+        LinkedList<byte> indexList = new LinkedList<byte>();
+
+        public EvilBot()
+        {
+            transpositiontable = Enumerable.Range(0, 256).Select(_ => new List<transposition>()).ToArray();
+        }
+
+        void setTransposition(transposition transposition)
+        {
+            int index = (int)transposition.zobristHash & 255;
+
+
+            transposition matchingTranspo = transpositiontable[index].Find(x => x.zobristHash == transposition.zobristHash);
+
+            if (!matchingTranspo.initialized)
+            {
+                indexList.AddLast((byte)index);
+                transpositiontable[index].Add(transposition);
+            }
+        }
+
+        transposition getTransposition(ulong zobristHash, int depth)
+        {
+            int index = (int)zobristHash & 255;
+
+
+            transposition matchingTranspo = transpositiontable[index].Find(x => x.zobristHash == zobristHash);
+
+            if (matchingTranspo.initialized && depth >= matchingTranspo.depth)
+            {
+                return matchingTranspo;
+            }
+
+            return new transposition();
+        }
+
+
+
+
+
+
+
         public Move Think(Board board, Timer timer)
         {
             this.board = board;
             Move[] moves = board.GetLegalMoves();
             moves = moves.OrderByDescending(m => (int)m.CapturePieceType).ToArray();
-            double highestEval = -10;
+            int highestEval = int.MinValue + 10;
             Random rng = new();
             List<Move> bestMoves = new List<Move>();
             foreach (Move move in moves)
             {
                 board.MakeMove(move);
-                double eval = -recursiveLookUp(5, -10f, 10f);
+                int eval = -recursiveLookUp(4, highestEval, int.MaxValue);
                 if (eval > highestEval)
                 {
                     highestEval = eval;
@@ -38,45 +99,36 @@ namespace ChessChallenge.Example
             }
             return bestMoves[rng.Next(bestMoves.Count)];
         }
-        double recursiveLookUp(int depth, double alpha, double beta)
+        int recursiveLookUp(int depth, int alpha, int beta)
         {
             if (board.IsInCheckmate())
             {
-                return -10;
+                return int.MinValue + 10;
             }
             else if (board.IsDraw())
             {
                 return 0;
             }
+            if (board.IsInCheck())
+            {
+                depth++;
+            }
 
             if (depth == 0)
             {
-
-                PieceList[] pieceLists = board.GetAllPieceLists();
-                int blackScore = 0;
-                int whiteScore = 0;
-                for (int i = 0; i < 12; i++)
-                {
-                    if (i > 5)
-                        blackScore += pieceLists[i].Count * values[(int)pieceLists[i].TypeOfPieceInList];
-                    else
-                        whiteScore += pieceLists[i].Count * values[(int)pieceLists[i].TypeOfPieceInList];
-
-                }
-                double whiteScoreUp = whiteScore - blackScore;
-                double score = 10d * Math.Tanh(whiteScoreUp / 32f);
-                return board.IsWhiteToMove ? score : -score;
+                return qSearch(alpha, beta);
             }
 
 
             Move[] moves = board.GetLegalMoves();
             moves = moves.OrderByDescending(m => (int)m.CapturePieceType).ThenBy(m => (int)m.MovePieceType).ToArray();
-            double eval;
+            int eval;
             foreach (Move move in moves)
             {
-
                 board.MakeMove(move);
+
                 eval = -recursiveLookUp(depth - 1, -beta, -alpha);
+
                 board.UndoMove(move);
                 if (eval >= beta)
                 {
@@ -86,6 +138,65 @@ namespace ChessChallenge.Example
                 alpha = Math.Max(eval, alpha);
 
             }
+            return alpha;
+        }
+
+        int qSearch(int alpha, int beta)
+        {
+            if (board.IsInCheckmate())
+            {
+                return int.MinValue + 10;
+            }
+            else if (board.IsDraw())
+            {
+                return 0;
+            }
+            PieceList[] pieceLists = board.GetAllPieceLists();
+            int blackScore = 0;
+            int whiteScore = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if (i > 5)
+                    blackScore += pieceLists[i].Count * getPieceValue(pieceLists[i].TypeOfPieceInList);
+                else
+                    whiteScore += pieceLists[i].Count * getPieceValue(pieceLists[i].TypeOfPieceInList);
+
+            }
+            int whiteScoreUp = whiteScore - blackScore;
+
+            int stand_pat = board.IsWhiteToMove ? whiteScoreUp : -whiteScoreUp;
+
+            if (stand_pat >= beta)
+                return beta;
+
+            alpha = Math.Max(stand_pat, alpha);
+
+            Move[] moves = board.GetLegalMoves(true);
+            if (moves.Length != 0)
+            {
+                moves = moves.OrderByDescending(m => (int)m.CapturePieceType).ThenBy(m => (int)m.MovePieceType).ToArray();
+                foreach (Move move in moves)
+                {
+                    /*if (getPieceValue(move.CapturePieceType) + 200 + stand_pat < alpha)
+                    {
+                        continue;
+                    }*/
+
+                    board.MakeMove(move);
+
+                    int eval = -qSearch(-beta, -alpha);
+                    board.UndoMove(move);
+                    if (eval >= beta)
+                    {
+
+                        return beta;
+                    }
+                    alpha = Math.Max(eval, alpha);
+
+                }
+                return alpha;
+            }
+
             return alpha;
         }
     }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,12 @@
 ﻿using ChessChallenge.API;
+//using ChessChallenge.Chess;
+//using ChessChallenge.Chess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class MyBot : IChessBot
-{   
+{
 
 
     Board board;
@@ -17,86 +19,45 @@ public class MyBot : IChessBot
     }
 
 
-    struct transposition
+    struct Transposition
     {
         public ulong zobristHash;
         public sbyte depth;
         public int eval;
-        public bool initialized;
-    }
-    
-    List<transposition>[] transpositiontable = new List<transposition>[256];
-
-    LinkedList<byte> indexList= new LinkedList<byte>();
-
-    public MyBot()
-    {
-        transpositiontable = Enumerable.Range(0, 256 ).Select(_ => new List<transposition>()).ToArray();
+        public byte flag;
     }
 
-    void setTransposition(transposition transposition)
-    {
-        int index = (int)transposition.zobristHash & 255;
-        
+    Transposition[] transpositiontable = new Transposition[8388608];
 
-        transposition matchingTranspo = transpositiontable[index].Find(x => x.zobristHash == transposition.zobristHash);
-
-        if (!matchingTranspo.initialized)
-        {
-            indexList.AddLast((byte)index);
-            transpositiontable[index].Add(transposition);
-        }
-    }
-
-    transposition getTransposition(ulong zobristHash, int depth)
-    {
-        int index = (int)zobristHash & 255;
-
-
-        transposition matchingTranspo = transpositiontable[index].Find(x => x.zobristHash == zobristHash);
-
-        if (matchingTranspo.initialized && depth >= matchingTranspo.depth)
-        {
-            return matchingTranspo;
-        }
-
-        return new transposition();
-    }
-
-
-
-
+    int noTT;
+    int bestTTmatch;
+    bool betaCutOff;
 
 
 
     public Move Think(Board board, Timer timer)
     {
+        BitboardHelper.VisualizeBitboard(BitboardHelper.GetKingAttacks(board.GetKingSquare(true)));
         this.board = board;
         Move[] moves = board.GetLegalMoves();
         moves = moves.OrderByDescending(m => (int)m.CapturePieceType).ToArray();
-        int highestEval = int.MinValue + 10;
-        Random rng = new();
-        List<Move> bestMoves = new List<Move>();
+        int highestEval = int.MinValue + 2;
+        Move bestMove = Move.NullMove;
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int eval = -recursiveLookUp(4, highestEval, int.MaxValue);
+            int eval = -recursiveLookUp(4, int.MinValue +2, -highestEval);
             if (eval > highestEval)
-            {
+            {   
                 highestEval = eval;
-                bestMoves.Clear();
-                bestMoves.Add(move);
-            }
-            else if (eval == highestEval)
-            {
-                bestMoves.Add(move);
+                bestMove = move;
             }
             board.UndoMove(move);
         }
-        return bestMoves[rng.Next(bestMoves.Count)];
+        return bestMove;
     }
     int recursiveLookUp(int depth, int alpha, int beta)
-    {   
+    {
         if (board.IsInCheckmate())
         {
             return int.MinValue + 10;
@@ -111,50 +72,52 @@ public class MyBot : IChessBot
         }
 
         if (depth == 0)
-        {
-            return qSearch(alpha, beta);
+        {   int q = qSearch(alpha, beta);
+            return q;
         }
 
 
-        Move[] moves = board.GetLegalMoves();
-        moves = moves.OrderByDescending(m => (int)m.CapturePieceType).ThenBy(m => (int)m.MovePieceType).ToArray();
-        int eval;
+        var (moves, TTeval, alphabetaCutoff) = orderAndcheckForTranspos(alpha, beta, depth, board.GetLegalMoves());
+        if (alphabetaCutoff)
+            return TTeval;
+        else
+            alpha = Math.Max(TTeval, alpha);
         foreach (Move move in moves)
         {
             board.MakeMove(move);
 
-            transposition transposition = getTransposition(board.ZobristKey, depth);
-            if (transposition.initialized)
-            {   
-                eval = transposition.eval;
-                Console.WriteLine("yaaay");
-            }else
-            {
-                eval = -recursiveLookUp(depth - 1, -beta, -alpha);
-                setTransposition(new transposition
-                {
-                    depth = (sbyte)depth,
-                    eval = eval,
-                    initialized = true,
-                    zobristHash = board.ZobristKey
-                });
-                
-            }
+            int eval = -recursiveLookUp(depth - 1, -beta, -alpha);
+
+
+            ref Transposition transpo = ref transpositiontable[board.ZobristKey & 4095];
+            transpo.depth = (sbyte)depth;
+            transpo.zobristHash = board.ZobristKey;
+            transpo.eval = eval;
 
             board.UndoMove(move);
             if (eval >= beta)
             {
-
+                transpo.flag = 2;
                 return beta;
             }
-            alpha = Math.Max(eval, alpha);
-
+            else if (eval >= alpha)
+            {
+                transpo.flag = 3;
+                alpha = eval;
+            }
+            else
+            {
+                transpo.flag = 1;
+            }
         }
         return alpha;
     }
 
     int qSearch(int alpha, int beta)
     {
+        bool isWhiteToMove = board.IsWhiteToMove;
+
+
         if (board.IsInCheckmate())
         {
             return int.MinValue + 10;
@@ -169,18 +132,37 @@ public class MyBot : IChessBot
         for (int i = 0; i < 12; i++)
         {
             if (i > 5)
-                blackScore += pieceLists[i].Count * getPieceValue(pieceLists[i].TypeOfPieceInList);
+                blackScore += pieceEval(pieceLists[i], false);
             else
-                whiteScore += pieceLists[i].Count * getPieceValue(pieceLists[i].TypeOfPieceInList);
-
+                whiteScore += pieceEval(pieceLists[i], true);
         }
         int whiteScoreUp = whiteScore - blackScore;
 
-        int stand_pat = board.IsWhiteToMove ? whiteScoreUp : -whiteScoreUp;
+        int stand_pat = isWhiteToMove ? whiteScoreUp : -whiteScoreUp;
+
+
+
+
+        //easy endgames
+        if ((isWhiteToMove ? blackScore : whiteScore) < 299)
+        {
+            int endgameeval = 0;
+            int file = board.GetKingSquare(!isWhiteToMove).File;
+            int rank = board.GetKingSquare(!isWhiteToMove).Rank;
+            endgameeval += Math.Max(3- rank,rank - 4);
+            endgameeval += Math.Max(3 - file, file - 4);
+
+            int endgameeval2 = (14 - Math.Abs((board.GetKingSquare(isWhiteToMove).Rank - board.GetKingSquare(!isWhiteToMove).Rank)) - Math.Abs((board.GetKingSquare(isWhiteToMove).File - board.GetKingSquare(!isWhiteToMove).File)));
+            
+            stand_pat += endgameeval*20 + endgameeval2*20;
+
+        }
+
+        
 
         if (stand_pat >= beta)
             return beta;
-
+        
         alpha = Math.Max(stand_pat, alpha);
 
         Move[] moves = board.GetLegalMoves(true);
@@ -211,4 +193,84 @@ public class MyBot : IChessBot
 
         return alpha;
     }
+
+
+
+    int pieceEval(PieceList pieceList, bool isWhite)
+    {
+        int val = 0;
+        foreach (Piece piece in pieceList)
+        {
+            val += BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetPieceAttacks(pieceList.TypeOfPieceInList, piece.Square, board, isWhite));
+            if (pieceList.TypeOfPieceInList == PieceType.Pawn)
+            {
+                val += (int)Math.Pow((isWhite ? piece.Square.Rank : 7 - piece.Square.Rank),3)/20;
+            }
+
+        }
+        return pieceList.Count * getPieceValue(pieceList.TypeOfPieceInList) + val*4;
+    }
+
+    (Move[], int, bool) orderAndcheckForTranspos(int alpha, int beta, int depth, Move[] moves)
+    {
+        noTT = 0;
+        bestTTmatch = int.MinValue + 10;
+        betaCutOff = false;
+
+        Move[] moves2 = moves.OrderByDescending(
+            move =>
+            {
+                if (betaCutOff)
+                    return -1;
+
+                board.MakeMove(move);
+
+                ref Transposition transposition = ref transpositiontable[board.ZobristKey & 8388607];
+
+                if (transposition.zobristHash == board.ZobristKey)
+                {
+                    if (transposition.depth >= depth)
+                    {
+                        if (transposition.flag == 1)
+                        {
+                            bestTTmatch = Math.Max(bestTTmatch, transposition.eval);
+                            board.UndoMove(move);
+                            return int.MinValue;
+                        }
+                        else if (transposition.flag == 2 && transposition.eval >= beta)
+                        {
+                            bestTTmatch = transposition.eval;
+                            betaCutOff = true;
+                            board.UndoMove(move);
+                            return int.MinValue;
+                        }
+                        else if (transposition.flag == 3 && transposition.eval <= alpha)
+                        {
+                            board.UndoMove(move);
+                            return int.MinValue;
+                        }
+                    }
+                    else
+                    {
+
+                        noTT++;
+                        board.UndoMove(move);
+                        return transposition.eval + (int)move.CapturePieceType * 10 - (int)move.MovePieceType;
+                    }
+                }
+                noTT++;
+                board.UndoMove(move);
+                //move ordering hier improoven
+                return (int)move.CapturePieceType * 10 - (int)move.MovePieceType;
+
+
+            }).ToArray();
+
+
+        return (moves2.Take(noTT).ToArray(), bestTTmatch, betaCutOff);
+    }
 }
+
+
+
+
